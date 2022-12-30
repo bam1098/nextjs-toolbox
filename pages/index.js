eaa770c4273375818624b49f5c433c7102b24748
bam1098 <bmcfalls1@gmail.com>
2022-12-30 10:38:31 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,6 @@ export default function Home() {
           Here's an example of a Netlify Form! When you fill this out, the
           submissions can be found in the Netlify Admin site.
         </p>
-        <JokeBlock />
         <FeedbackForm />
         <JokeBlock />
       </main>
