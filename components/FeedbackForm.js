c1aed916e3249742aae5cca446e2ac2be217d930
bam1098 <bmcfalls1@gmail.com>
2023-01-11 12:53:34 -0500

    Update FeedbackForm.js

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -1,62 +1,5 @@
 import styles from './FeedbackForm.module.css'
 
-var currentTab = 0;
-
-showTab(currentTab); // Display the current tab
-
-function showTab(n) {
-  if (process.browser){
-    // This function will display the specified tab of the form...
-    var x = document.getElementsByClassName("tab");
-    x[n].style.display = "block";
-    //... and fix the Previous/Next buttons:
-    if (n == 0) {
-      document.getElementById("prevBtn").style.display = "none";
-    } else {
-      document.getElementById("prevBtn").style.display = "inline";
-    }
-    if (n == (x.length - 1)) {
-      document.getElementById("nextBtn").innerHTML = "Submit";
-    } else {
-      document.getElementById("nextBtn").innerHTML = "Next";
-    }
-    //... and run a function that will display the correct step indicator:
-    fixStepIndicator(n)
-  };
-  
-}
-
-function btnStep(n) {
-  if (process.browser){
-    // This function will figure out which tab to display
-    var x = document.getElementsByClassName("tab");
-    // Exit the function if any field in the current tab is invalid:
-    if (n == 1 && !validateForm()) return false;
-    // Hide the current tab:
-    x[currentTab].style.display = "none";
-    // Increase or decrease the current tab by 1:
-    currentTab = currentTab + n;
-    // if you have reached the end of the form...
-    if (currentTab >= x.length) {
-      // ... the form gets submitted:
-      document.getElementById("regForm").submit();
-      return false;
-    }
-    // Otherwise, display the correct tab:
-    showTab(currentTab);
-  };
-}
-
-function fixStepIndicator(n) {
-  // This function removes the "active" class of all steps...
-  var i, x = document.getElementsByClassName("step");
-  for (i = 0; i < x.length; i++) {
-    x[i].className = x[i].className.replace(" active", "");
-  }
-  //... and adds the "active" class on the current step:
-  x[n].className += " active";
-}
-
 export default function FeedbackForm() {
   return (
       <form
@@ -88,9 +31,6 @@ export default function FeedbackForm() {
           <label htmlFor="feedback">What is your feedback?</label>
           <textarea id="feedback" className={styles['form-field']} wrap="soft" name="feedback" required></textarea>
         </div>
-
-        <button type="button" id="prevBtn" onclick="btnStep(-1)">Previous</button>
-        <button type="button" id="nextBtn" onclick="btnStep(1)">Next</button>
 
         <button className={styles.button} type="submit">Submit</button>
       </form>
