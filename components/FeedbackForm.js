9bb453ea0686825f10780887625b24223961e118
bam1098 <bmcfalls1@gmail.com>
2023-01-11 11:19:56 -0500

    Update FeedbackForm.js

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -16,15 +16,36 @@ export default function FeedbackForm() {
             Don’t fill this out if you’re human: <input name="bot-field" />
             </label>
         </p>
-  
-        <label htmlFor="name">Name</label>
-        <input id="name" className={styles['form-field']} type="text" name="name" />
+        <div class="tab" Meat:>
+          <label htmlFor="meat">Meat</label>
+          <p>How often do you eat animal-based products?</p>
+          <input id="meat" className={styles['form-field']} type="text" name="name" />
+        </div>
+            
+        <div class="tab" Heating:>
+          <label htmlFor="email">Email</label>
+          <input id="email" className={styles['form-field']} type="email" name="email" required />
+        </div>
 
-        <label htmlFor="email">Email</label>
-        <input id="email" className={styles['form-field']} type="email" name="email" required />
+        <div class="tab" Heating:>
+          <label htmlFor="feedback">What is your feedback?</label>
+          <textarea id="feedback" className={styles['form-field']} wrap="soft" name="feedback" required></textarea>
+        </div>
 
-        <label htmlFor="feedback">What is your feedback?</label>
-        <textarea id="feedback" className={styles['form-field']} wrap="soft" name="feedback" required></textarea>
+        <div style="overflow:auto;">
+          <div style="float:right;">
+            <button type="button" id="prevBtn" onclick="btnStep(-1)">Previous</button>
+            <button type="button" id="nextBtn" onclick="btnStep(1)">Next</button>
+          </div>
+        </div>
+
+        <!-- Circles which indicates the steps of the form: -->
+        <div style="text-align:center;margin-top:40px;">
+          <span class="step"></span>
+          <span class="step"></span>
+          <span class="step"></span>
+          <span class="step"></span>
+        </div>
         <button className={styles.button} type="submit">Submit</button>
       </form>
   )
