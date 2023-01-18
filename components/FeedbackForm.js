49f1506bbd52100e27e5ead33a8722151d1944b8
bam1098 <bmcfalls1@gmail.com>
2023-01-18 16:55:47 -0500

    Update FeedbackForm.js

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -34,7 +34,7 @@ export default function FeedbackForm() {
         <div class="tab" Meat>
           <label htmlFor="meat">Meat</label>
           <p id="a">How many times do you eat meat per week?</p>
-          <input id="meat" className={styles['form-field']} type="text" name="meat" onChange='handleChange()'/>
+          <input id="meat" className={styles['form-field']} type="text" name="meat" onChange={handleChange()}/>
         </div>
 
         <div class="tab" Driving>
