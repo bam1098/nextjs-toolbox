cecac4fcd0105acad36ca65d24665a5e2ed715c4
bam1098 <bmcfalls1@gmail.com>
2023-01-19 17:29:55 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,19 +8,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Toolbox</title>
+        <title>Footprint Calculator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Next.js Toolbox" />
+        <Header title="Footprint Calculator" />
         <hr />
-        <p className="description">
-          Here's an example of a Netlify Form! When you fill this out, the
-          submissions can be found in the Netlify Admin site.
-        </p>
         <FeedbackForm />
-        <JokeBlock />
       </main>
       <Footer />
     </div>
