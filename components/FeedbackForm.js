73d08d4364daf5283c7847f2f759b11db794af96
bam1098 <bmcfalls1@gmail.com>
2023-01-19 15:47:42 -0500

    Update FeedbackForm.js

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -35,34 +35,7 @@ export default function FeedbackForm() {
     document.getElementById("emissions").innerHTML = totEmissions;
   }
   
-  //HERE
-
-  function showTab(n) {
-    // This function will display the specified tab of the form ...
-    var x = document.getElementsByClassName("tab");
-    x[n].style.display = "block";
-  }
-
-  function nextPrev(n) {
-    var currentTab = 0
-    // This function will figure out which tab to display
-    var x = document.getElementsByClassName("tab");
-    // Exit the function if any field in the current tab is invalid:
-    if (n == 1 && !validateForm()) return false;
-    // Hide the current tab:
-    x[currentTab].style.display = "none";
-    // Increase or decrease the current tab by 1:
-    currentTab = currentTab + n;
-    // if you have reached the end of the form... :
-    if (currentTab >= x.length) {
-      //...the form gets submitted:
-      document.getElementById("regForm").submit();
-      return false;
-    }
-    // Otherwise, display the correct tab:
-    showTab(currentTab);
-  }
-  //here
+ 
   
   return (
       <form
@@ -133,12 +106,7 @@ export default function FeedbackForm() {
           </div>
         </div>
 
-        <div style="text-align:center;margin-top:40px;">
-          <span class="step"></span>
-          <span class="step"></span>
-          <span class="step"></span>
-          <span class="step"></span>
-        </div>
+
 
         <p className={styles['form-field']}>Total emissions: <span id="emissions"></span></p>
         <button className={styles.button} type="submit">Submit</button>
