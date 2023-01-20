21330dbcd9c5d447ce87718bb4d98a189600613f
bam1098 <bmcfalls1@gmail.com>
2023-01-19 19:51:49 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Footer from "@components/Footer";
-import background from "./public/background.jfif"
+import background from "/public/background.jfif"
 
 export default function Home() {
   return (
