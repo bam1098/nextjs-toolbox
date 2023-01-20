ac7fdecc0be2ee969679165e6579b6ad4e159914
bam1098 <bmcfalls1@gmail.com>
2023-01-19 19:23:29 -0500

    Update survey.js

--- pages/survey.js
+++ pages/survey.js
@@ -1,1 +1,17 @@
 import FeedbackForm from "@components/FeedbackForm";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Footprint Calculator</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <FeedbackForm />
+      </main>
+    </div>
+  );
+}
