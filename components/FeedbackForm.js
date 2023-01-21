aa64e21c9e6e4b60bbc009f8bfd2654ee5e93c96
bam1098 <bmcfalls1@gmail.com>
2023-01-21 11:33:32 -0500

    Update FeedbackForm.js

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -91,6 +91,7 @@ export default function FeedbackForm() {
         name="feedback"
         method="POST"
         action="/success"
+        style={{ backgroundImage:`url(${image})`,backgroundRepeat:"no-repeat",backgroundSize:"contain" }}
       >
         <input type="hidden" name="form-name" value="feedback" />
         <p className={styles.hidden}>
