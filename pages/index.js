6034eacfd06097c016a72c1429e452eb916a1fab
bam1098 <bmcfalls1@gmail.com>
2023-01-22 16:35:43 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,30 @@ import Footer from "@components/Footer";
 import FeedbackForm from '@components/FeedbackForm';
 import background from './bg-blank.svg';
 
-const styling = {
+const backgroundStyling = {
     backgroundImage: `url(${background.src})`,
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
     width:"100vw",
     height:"100vh",
 }
+const logo = {
+    height:"50px",
+    width:"50px",
+    
+}
+
 
 export default function Home() {
   return (
-    <div className="container" style={styling}>
+    <div className="container" style={backgroundStyling}>
       <Head>
         <title>Footprint Calculator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <img src='./ram.svg' alt="Rammy"/>
+        <img src='./ram.svg' alt="Rammy" align:"left" style={logo}/>
         <FeedbackForm />
       </main>
     </div>
