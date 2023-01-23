b7007d0d781766f52629c5f7a1ac83dcb0be5604
bam1098 <bmcfalls1@gmail.com>
2023-01-23 17:32:37 -0500

    Update success.js

--- pages/success.js
+++ pages/success.js
@@ -1,11 +1,25 @@
 import Head from 'next/head'
 import Link from 'next/link'
-
 import Footer from '@components/Footer'
+import background from './bgblank.svg'
+
+const backgroundStyling = {
+    backgroundImage: `url(${background.src})`,
+    backgroundSize: 'cover',
+    width:"100vw",
+    height:"140vh",
+}
+const logo = {
+    height:"13vh",
+    width:"13vw",
+    display:"flex",
+    justifyContent: "center",
+    padding:"1vh"
+}
 
 export default function Success() {
   return (
-    <div className="container">
+    <div className="container" style={backgroundStyling}>
       <Head>
         <title>Next.js Toolbox Template!</title>
         <link rel="icon" href="/favicon.ico" />
