cd4f93f39c3f3aa3d972ec4ec1cebd64db4965dc
bam1098 <bmcfalls1@gmail.com>
2023-01-23 15:28:17 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,18 +19,20 @@ const logo = {
 
 
 export default function Home() {
-  return (
-    <div className="container" style={backgroundStyling}>
-      <Head>
-        <title>Footprint Calculator</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <img src='./ram.svg' alt="Rammy" style={logo}/>
-      <main>
+    return (
+        <div>
+            <div style={backgroundStyling}></div>
+            <div className="container">
+              <Head>
+                <title>Footprint Calculator</title>
+                <link rel="icon" href="/favicon.ico" />
+              </Head>
+              <img src='./ram.svg' alt="Rammy" style={logo}/>
+              <main>
 
-        <FeedbackForm />
-      </main>
-    </div>
-
+                <FeedbackForm />
+              </main>
+            </div>
+        </div>
   );
 }
