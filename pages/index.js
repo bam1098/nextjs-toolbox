d2dd06ab49d77a16818ecf671e83912a9138ffed
bam1098 <bmcfalls1@gmail.com>
2023-01-23 15:02:24 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const backgroundStyling = {
     backgroundImage: `url(${background.src})`,
     backgroundSize: 'cover',
     width:"100vw",
-    height:"125vh",
+    height:"130vh",
 }
 const logo = {
     height:"10vh",
